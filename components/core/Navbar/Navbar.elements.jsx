c6dffa4bdc54664d65795199cfd04324d08c53d4
arogya <arogyabichpuria16@gmail.com>
2022-03-06 22:14:42 +0530

    created the navbar comp

---
+++ components/core/Navbar/Navbar.elements.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const NavContainer = styled.nav`
+    display:flex; 
+    flex-flow:row wrap; 
+    justify-content:space-around;
+
+`
+
+export const WebsiteName = styled.h1`
+    font-size:1.5rem; 
+    font-weight:bold;
+    font-family:sans-serif;
+`
---
+++ components/core/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavContainer, WebsiteName } from './Navbar.elements'
+
+const Navbar = () => {
+  return (
+    <NavContainer>
+     <WebsiteName>Shortly</WebsiteName> 
+    </NavContainer>
+  )
+}
+
+export default Navbar
