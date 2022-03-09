de826fbc48315c52e8f7f6af50f9435e6899fef2
arogya <arogyabichpuria16@gmail.com>
2022-03-09 14:42:59 +0530

    :lipstick: implemented the custom hook for copying on the clipboard

--- components/core/PastLinks/PastLinks.jsx
+++ components/core/PastLinks/PastLinks.jsx
@@ -4,13 +4,16 @@ import {
   CopyBtn,
   PastLinkWrapper,
   ShortedLinksWrapper,
-  ShortedLinksWrapperBtn
+  ShortedLinksWrapperBtn,
 } from "./PastLinks.elements";
-import React from 'react'
-import uuid from 'react-uuid'
+import React from "react";
+import uuid from "react-uuid";
+
+import useCopyToClipboard from "../../../hooks/useClipboard";
 
 export default function PastLinks() {
   const { shortedLinks, setShortenedLinks } = useContext(LinkContext);
+  const [isCopied, handleCopy] = useCopyToClipboard();
 
   useEffect(() => {
     let retrieveItems = localStorage.getItem("inputLink");
@@ -19,9 +22,9 @@ export default function PastLinks() {
       retrieveItems = JSON.parse(retrieveItems);
       // console.log(retrieveItems);
 
-      retrieveItems.map(el => {
-        setShortenedLinks(shortedLinks => [...shortedLinks,el]);
-      })
+      retrieveItems.map((el) => {
+        setShortenedLinks((shortedLinks) => [...shortedLinks, el]);
+      });
     }
   }, [setShortenedLinks]);
 
@@ -35,7 +38,15 @@ export default function PastLinks() {
                 <ShortedLinksWrapper>{el[0]}</ShortedLinksWrapper>
                 <ShortedLinksWrapperBtn>
                   {el[1]}
-                  <CopyBtn>Copy</CopyBtn>
+                  <CopyBtn
+                    onClick={(event) => {
+                      handleCopy(el[1]);
+
+                      event.target.innerHTML = "Copied!!";
+                    }}
+                  >
+                    Copy
+                  </CopyBtn>
                 </ShortedLinksWrapperBtn>
               </PastLinkWrapper>
             );
