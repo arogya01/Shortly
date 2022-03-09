26e7280d119abad74102b48323c2862bb0c4366d
arogya <arogyabichpuria16@gmail.com>
2022-03-09 12:22:19 +0530

    added a few contexts to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,19 @@
 import { ThemeProvider } from 'styled-components'
+import ShortenedLinkProvider from '../context/ShortenedLinksContext'
 import '../styles/globals.css'
+import GlobalSpinnerProvider from '../context/globalSpinnerContext';
+
 // import {theme} from '../themes';
 
 function MyApp({ Component, pageProps }) {
 
   return (
-   
-   <Component {...pageProps} />  
-    
+   <GlobalSpinnerProvider>
+   <ShortenedLinkProvider>
+   <Component {...pageProps} />
+   </ShortenedLinkProvider>
+   </GlobalSpinnerProvider>
+       
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import dynamic from 'next/dynamic'
 import Head from 'next/head'
 import Image from 'next/image'
+import Features from '../components/core/Features/Features'
 import HeroSection from '../components/core/HeroSection/HeroSection'
+import PastLinks from '../components/core/PastLinks/PastLinks'
+import Shortner from '../components/core/Shortener/Shortner'
 // import Navbar from '../components/core/Navbar/Navbar'
 
 const Navbar = dynamic(
@@ -17,6 +20,9 @@ export default function Home() {
        <Navbar />
 
        <HeroSection/>
+       <Shortner />
+       <PastLinks />
+       <Features />
        </>
   )
 }
