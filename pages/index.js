45680f33a9ca3f5d2ed950a09dc7d10ecba0383d
arogya <arogyabichpuria16@gmail.com>
2022-03-09 13:58:24 +0530

    minor changes

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Features from '../components/core/Features/Features'
 import HeroSection from '../components/core/HeroSection/HeroSection'
 import PastLinks from '../components/core/PastLinks/PastLinks'
 import Shortner from '../components/core/Shortener/Shortner'
+import Footer from '../components/core/Footer/Footer'
 // import Navbar from '../components/core/Navbar/Navbar'
 
 const Navbar = dynamic(
@@ -23,6 +24,7 @@ export default function Home() {
        <Shortner />
        <PastLinks />
        <Features />
+       <Footer />
        </>
   )
 }
